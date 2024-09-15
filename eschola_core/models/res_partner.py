737c999851100a72394842093fb79631beff42d1
izumiofficial <izurutsuki@gmail.com>
2024-09-15 13:47:13 +0800

    [FIX] error

--- eschola_core/models/res_partner.py
+++ eschola_core/models/res_partner.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api, _
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     admission_register_id = fields.Many2one('admission.register', string="Admission Register")
     # is_activated = fields.Boolean(string='Is Activated', default=False)
