4214049761dfc87b8d8ee09ccdd0a402582d8535
izumiofficial <izurutsuki@gmail.com>
2024-09-15 14:15:24 +0800

    [FIX] manifest

--- eschola_core/models/res_partner.py
+++ eschola_core/models/res_partner.py
@@ -1,52 +1,48 @@
 from odoo import models, fields, api, _
 
 class ResPartner(models.Model):
-    _inherit = "res.partner"
+    _inherit = 'res.partner'
 
     admission_register_id = fields.Many2one('admission.register', string="Admission Register")
-    # is_activated = fields.Boolean(string='Is Activated', default=False)
 
-    # gender = fields.Selection([
-    #     ('male', 'Male'),
-    #     ('female', 'Female'),
-    # ], string='Gender')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], string='Gender')
 
     std_created = fields.Boolean(string='Student Created', default=False)
     gdn_created = fields.Boolean(string='Guardian Created', default=False)
+    flak = fields.Char(string='FLAK')
+
+    def _create_portal_user(self, partner_id, email):
+        """Helper function to create a portal user."""
+        return self.env['res.users'].sudo().create({
+            'login': email,
+            'partner_id': partner_id,
+            'groups_id': [(6, 0, [self.env.ref('base.group_portal').id])],
+        })
 
     def action_create_student(self):
         for record in self:
-            record.env['student'].create({  # Assuming 'admission.application' is the model in admission.py
+            student = record.env['student'].create({
                 'name': record.name,
                 'email': record.email,
                 'mobile': record.mobile,
-                # 'gender': record.gender,
+                'gender': record.gender,
                 'country': record.country.id,
             })
 
-            user = record.env['res.users'].sudo().create({
-                'login': record.email,
-                'partner_id': record.partner.id,
-                'groups_id': [(6, 0, [record.env.ref('base.group_portal').id])],
-            })
-
-            # set std_created to True
+            self._create_portal_user(student.partner_id.id, record.email)
             record.std_created = True
 
     def action_create_guardian(self):
         for record in self:
-            record.env['guardian'].create({  # Assuming 'admission.application' is the model in admission.py
+            guardian = record.env['guardian'].create({
                 'name': record.name,
                 'email': record.email,
                 'mobile': record.mobile,
                 'country': record.country.id,
             })
 
-            user = record.env['res.users'].sudo().create({
-                'login': record.email,
-                'partner_id': record.partner.id,
-                'groups_id': [(6, 0, [record.env.ref('base.group_portal').id])],
-            })
-
-            # set gdn_created to True
+            self._create_portal_user(guardian.partner_id.id, record.email)
             record.gdn_created = True
