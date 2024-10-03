cce8a41c182619942bd8bda564f696667492f0e9
izumiofficial <izurutsuki@gmail.com>
2024-10-03 16:47:02 +0800

    [IMP] View attendance from student model

--- eschola_core/models/guardian.py
+++ eschola_core/models/guardian.py
@@ -14,4 +14,32 @@ class Guardian(models.Model):
     country = fields.Many2one('res.country', string='Country of Residence')
 
     student_ids = fields.One2many('student', 'primary_guardian_id', string='Child')
-    other_guardian = fields.Many2one() # in
+    # other_guardian = fields.Many2one()
+
+    @api.onchange('name')
+    def _update_name(self):
+        # if name in guardian is change, name in res.partner also change/updated
+        for record in self:
+            if record.partner_id:
+                record.partner_id.name = record.name
+
+    @api.onchange('country')
+    def _update_country(self):
+        # if country in guardian is change, country in res.partner also change/updated
+        for record in self:
+            if record.partner_id:
+                record.partner_id.country_id = record.country
+
+    @api.onchange('email')
+    def _update_email(self):
+        # if email in guardian is change, email in res.partner also change/updated
+        for record in self:
+            if record.partner_id:
+                record.partner_id.email = record.email
+
+    @api.onchange('mobile')
+    def _update_mobile(self):
+        # if mobile in guardian is change, mobile in res.partner also change/updated
+        for record in self:
+            if record.partner_id:
+                record.partner_id.mobile = record.mobile
